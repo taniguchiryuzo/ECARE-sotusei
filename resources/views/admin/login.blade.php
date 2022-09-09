7f901743f7279a29cde3e3700910c660b51d5d6d
taniguchiryuzo <kousei0038@gmail.com>
2022-09-09 00:59:41 +0000

    type"修正

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,9 +4,9 @@
   <title>管理者ログイン</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
-  <link rel="stylesheet" href="css/admin/tailwind/tailwind.min.css" type="text/css>
+      <!-- <link rel="stylesheet" href="https://rsms.me/inter/inter.css"> -->
+  <link rel="stylesheet" href="css/admin/tailwind/tailwind.min.css" type="text/css">
+  
 
 
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png">
