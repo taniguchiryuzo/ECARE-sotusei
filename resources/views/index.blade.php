a3c21085cddec773a574b2978a067b1716ab806e
taniguchiryuzo <kousei0038@gmail.com>
2022-09-09 16:17:27 +0000

    スペース

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -86,6 +86,7 @@
                         <img class="w-12 h-12 mr-4 object-cover rounded-md" src="{{ asset('/storage/app/public/'.$recipi->image) }}" alt="">
                         <p class="font-medium"><a href="{{ route('detail',['recipi' => $recipi->id]) }}">{{ $recipi->title }}</a></p>
                     </td>
+                    
                     <td class="font-medium">{{ $recipi->type }}</td>
                     <td class="font-medium">{{ $recipi->foodform }}</td>
                     {{-- <td>{{ $recipi->updated_at }}</td> --}}
