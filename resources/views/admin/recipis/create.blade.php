df25322389d5f3bc64bffb25bda3b3ebb70b95d3
taniguchiryuzo <kousei0038@gmail.com>
2022-09-09 03:28:35 +0000

    ..追加

--- resources/views/admin/recipis/create.blade.php
+++ resources/views/admin/recipis/create.blade.php
@@ -38,7 +38,7 @@
                     <div class="mb-6">
                         <label class="block text-sm font-medium mb-2" for="image">画像</label>
                         <div class="flex items-end">
-                            <img id="previewImage" src="/images/admin/noimage.jpg" data-noimage="/images/admin/noimage.jpg" alt="" class="rounded shadow-md w-64">
+                            <img id="previewImage" src="../images/admin/noimage.jpg" data-noimage="/images/admin/noimage.jpg" alt="" class="rounded shadow-md w-64">
                             <input id="image" class="block w-full px-4 py-3 mb-2" type="file" accept='image/*' name="image">
                         </div>
                     </div>
