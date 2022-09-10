0dbca7e77f76383a3d8f75a96650bddefb970ed0
taniguchiryuzo <kousei0038@gmail.com>
2022-09-10 12:36:15 +0000

    font変更

--- resources/views/admin/recipis/create.blade.php
+++ resources/views/admin/recipis/create.blade.php
@@ -36,13 +36,13 @@
                     </div>
 
                     <div class="mb-6">
-                        <label class="block text-sm font-medium mb-2" for="image">画像</label>
+                        <label class="block text-sm font-medium mb-1" for="image">画像</label>
                         <div class="flex items-end">
                             <img id="previewImage" src="../../images/admin/noimage.jpg" data-noimage="../..//images/admin/noimage.jpg" alt="" class="rounded shadow-md w-64">
                         </div>
-                    <div>
-                        <input id="image" class="block w-full px-4 py-3 mb-2" type="file" accept='image/*' name="image">
-                    </div>
+                        <div>
+                            <input id="image" class="block w-full px-4 py-3 mb-2" type="file" accept='image/*' name="image">
+                        </div>
                     </div>
 
                     <div class="mb-6">
