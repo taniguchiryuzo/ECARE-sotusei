c601d9bf002e5dad80a28c6cca56301b1e0e3b73
taniguchiryuzo <kousei0038@gmail.com>
2022-09-14 13:18:50 +0000

    最下部のscript削除

--- resources/views/admin/recipis/create.blade.php
+++ resources/views/admin/recipis/create.blade.php
@@ -110,20 +110,4 @@
     </div>
 </section>
 
-<script>
-    // ねこちゃんたち追加
-    $('#js-pulldown').select2();
-
-    // 画像プレビュー
-    document.getElementById('image').addEventListener('change', e => {
-        const previewImageNode = document.getElementById('previewImage')
-        const fileReader = new FileReader()
-        fileReader.onload = () => previewImageNode.src = fileReader.result
-        if (e.target.files.length > 0) {
-            fileReader.readAsDataURL(e.target.files[0])
-        } else {
-            previewImageNode.src = previewImageNode.dataset.noimage
-        }
-    })
-</script>
 @endsection
